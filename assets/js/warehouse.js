d6c64757ae15afefa4029cb76a91c4e8b2e6327c
patrick-o-code <pratikyella40@gmail.com>
2024-12-05 13:59:54 +0100

    Rollback JS Prevent body from scrolling when colorBox open

--- assets/js/warehouse.js
+++ assets/js/warehouse.js
@@ -186,13 +186,6 @@ var ColorBox = function() {
 		minHeight: 153,
 		scrolling: true
 	});
-
-	// @since 12.0.2 Prevent body from scrolling when colorBox open
-	$(document).bind('cbox_open', function() {
-		$('body').css({ overflow: 'hidden' });
-	}).bind('cbox_closed', function() {
-		$('body').css({ overflow: '' });
-	});
 }
 
 // MarkDown.
