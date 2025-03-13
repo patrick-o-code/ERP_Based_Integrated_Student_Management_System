ad51ba6a964c598572c42e693e2966b0c5abcfbc
patrick-o-code <pratikyella40@gmail.com>
2025-03-13 18:25:04 +0100

    Fix PHPDoc comment, PopTable() return void

--- functions/PopTable.fnc.php
+++ functions/PopTable.fnc.php
@@ -18,10 +18,10 @@
  * @since 10.6 CSS responsive add .postbox-wrapper class for overflow-x scroll
  *
  * @param  string       $action        'header' or 'footer'.
- * @param  string|array $tabs_or_title PopTable Title or Tabs: array( array( 'link' => 'tab-link.php', 'title' => 'Tab Title' ) ) (optional).
+ * @param  string|array $tabs_or_title PopTable Title or Tabs: [ [ 'link' => 'tab-link.php', 'title' => 'Tab Title' ] ] (optional).
  * @param  string       $table_att     <table [attributes]> (optional).
  *
- * @return string outputs PopTable HTML
+ * @return void Outputs PopTable HTML
  */
 function PopTable( $action, $tabs_or_title = 'Search', $table_att = '' )
 {
