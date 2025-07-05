46b191f44833f7347a20ae66c054527d7a6c8ada
patrick-o-code <pratikyella40@gmail.com>
2025-07-05 13:25:34 +0200

    Fix Add noreferrer attribute to index page links

--- index.php
+++ index.php
@@ -583,7 +583,7 @@ if ( empty( $_SESSION['STAFF_ID'] )
 		</p>
 		<p class="center size-1">
 			&copy; 2004-2009 The Miller Group &amp; Learners Circle
-			<br />&copy; 2012-2025 <a href="https://www.rosariosis.org" noreferrer>RosarioSIS</a>
+			<br />&copy; 2012-2025 <a href="https://www.rosariosis.org" rel="noreferrer">RosarioSIS</a>
 		</p>
 	</details>
 
